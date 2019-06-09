4040f2e0caac64b736df004675bb71257a6ab649
Rohan van Wijk <rohan_10@live.nl>
2019-06-09 23:37:58 +0200

    gulp added and more

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+
+function pugg() {
+    return gulp.src('./pugs/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./'))
+}
+
+// compile scss to css
+function style() {
+    // location scss files
+    return gulp.src('./scss/**/*.scss')
+    //  pass to sass compiler
+    .pipe(sass().on('error', sass.logError))
+    // destination css file
+    .pipe(gulp.dest('./css'))
+    // stream sync all browsers
+    //.pipe(browserSync.stream())
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+   
+    gulp.watch('./scss/**/*.scss', style)
+    gulp.watch('./*.html').on('change', browserSync.reload)
+    gulp.watch('./css/*.css').on('change', browserSync.reload)
+    gulp.watch('./pugs/**/*.pug', pugg)
+    //gulp.watch('./pugs/**/*.pug').on('change', browserSync.reload)
+}
+
+exports.style = style;
+exports.watch = watch;
+exports.pugg = pugg;
